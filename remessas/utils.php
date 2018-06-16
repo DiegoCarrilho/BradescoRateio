a635c8cd28fc3aa33b270766c3a58e90eeee1b7a
DiegoCarrilho <diegocarrilho@hotmail.com>
2018-06-16 10:48:06 +0100

    Update utils.php

--- remessas/utils.php
+++ remessas/utils.php
@@ -3,11 +3,9 @@
 class Utilidades {
 	
 	
-	   public function formataNumero($valor, $numCasasDecimais = 2) {
-        if ($valor == "") {
+    public function formataNumero($valor, $numCasasDecimais = 2) {
+        if ($valor == "") 
             return 0;
-        }
-        
         $casas = $numCasasDecimais;
         if ($casas > 0) {
             $valor = substr($valor, 0, strlen($valor) - $casas) . "." . substr($valor, strlen($valor) - $casas, $casas);
@@ -24,62 +22,51 @@ class Utilidades {
             $return = str_replace(",", "", $return);
 
             return $return;
-        } else {
+        } else 
             throw new Exception('Error - O valor ' . $valor . ' nao eh um numero.');
-        }
     }
 	
     public function juros_vencido($valor,$juro)
     {
         if (is_numeric($valor)) {
-			$juro_dia_floated			=	self::formataNumero($juro);
-			$valor_do_titulo_floated	=	self::formataNumero($valor);
-			return						self::removeFormatacaoMoeda((number_format($valor_do_titulo_floated* ($juro_dia_floated / 100),2)));
+		$juro_dia_floated		=	self::formataNumero($juro);
+		$valor_do_titulo_floated	=	self::formataNumero($valor);
+		return	self::removeFormatacaoMoeda((number_format($valor_do_titulo_floated* ($juro_dia_floated / 100),2)));
 
-        } else {
+        } else
             throw new Exception('Error - O valor ' . $valor . ' nao eh um numero.');
-        }
     }
 	
     public function cep_valida($valor)
     {
-		$return = preg_replace( '/[^0-9]/is', '', $valor );
+	$return = preg_replace( '/[^0-9]/is', '', $valor );
 		
         if (is_numeric($return)) {
             return substr($return,0,5);
-        } else {
-			die($valor);
-            //throw new Exception('Error - O valor ' . $valor . ' nao eh um numero.');
-        }
+        } else 
+            throw new Exception('Error - O valor ' . $valor . ' nao eh um numero.');
     }
 
     public function cep_sufixo($valor)
     {
-		$return = preg_replace( '/[^0-9]/is', '', $valor );
+	$return = preg_replace( '/[^0-9]/is', '', $valor );
 		
         if (is_numeric($return)) {
             return substr($return,5);
-        } else {
+        } else 
             throw new Exception('Error - O valor ' . $valor . ' nao eh um numero.');
-        }
     }
 	
-	   public function formataData($data) {
+    public function formataData($data) {
 		   
-        if ($data == "") {
+        if ($data == "") 
             throw new Exception('Error - O valor ' . $data . ' nao é uma data válida.');
-        }
 		  
-		if ($data == "0000-00-00") {
+	if ($data == "0000-00-00") {
             return '000000';
         }else
-		   
-        //formata a data, a partir do padrão americano, para o padrão DD/MM/AAAA
-        return date("dmy", strtotime($data));
-    }
-	
-	
-	
+       	 return date("dmy", strtotime($data));
+    }	
 }
 
 
